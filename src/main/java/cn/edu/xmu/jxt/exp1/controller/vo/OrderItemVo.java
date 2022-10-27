80ea69b9ec9367a297259dae823c14fa4d1a2ace
nooralu <nooralu@foxmail.com>
2022-10-27 19:13:52 +0800

    新增 OrderVo 和 OrderItemVo

---
+++ src/main/java/cn/edu/xmu/jxt/exp1/controller/vo/OrderItemVo.java
@@ -0,0 +1,37 @@
+package cn.edu.xmu.jxt.exp1.controller.vo;
+
+import cn.edu.xmu.jxt.exp1.dao.bo.OrderItem;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 订单明细视图对象
+ *
+ * @author Dasong Lu
+ */
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class OrderItemVo {
+
+    private Long skuId;
+
+    private Integer quantity;
+
+    private Long couponActId;
+
+    public OrderItemVo(OrderItem orderItem) {
+        this.skuId = orderItem.getGoodsSkuId();
+        this.quantity = orderItem.getQuantity();
+        this.couponActId = orderItem.getCouponActivityId();
+    }
+
+    public OrderItem createBo() {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setGoodsSkuId(this.skuId);
+        orderItem.setQuantity(this.quantity);
+        orderItem.setCouponActivityId(this.couponActId);
+        return orderItem;
+    }
+}
